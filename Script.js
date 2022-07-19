64f7e686d5ec1ee7afd3d4ffc956876e1c3fc570
logesh8220 <106301040+logesh8220@users.noreply.github.com>
2022-07-19 11:17:19 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,45 @@
+setTimeout(() => {
+    document.getElementById("p").innerHTML = 10;
+    setTimeout(() => {
+        document.getElementById("p").innerHTML = 9;
+        setTimeout(() => {
+            document.getElementById("p").innerHTML = 8;
+            setTimeout(() => {
+                document.getElementById("p").innerHTML = 7;
+                setTimeout(() => {
+                    document.getElementById("p").innerHTML = 6;
+                    setTimeout(() => {
+                        document.getElementById("p").innerHTML = 5;
+                        setTimeout(() => {
+                            document.getElementById("p").innerHTML = 4;
+                            setTimeout(() => {
+                                document.getElementById("p").innerHTML = 3;
+                                setTimeout(() => {
+                                    document.getElementById("p").innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.getElementById("p").innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.getElementById("p").innerHTML = "Happy Independence Day";
+                                        
+                                        }, 1000);
+                                    
+                                    }, 1000);
+                                
+                                }, 1000);
+                            
+                            }, 1000);
+                        
+                        }, 1000);
+                    
+                    }, 1000);
+                
+                }, 1000);
+            
+            }, 1000);
+        
+        }, 1000);
+    
+    }, 1000);
+
+}, 1000);
+
